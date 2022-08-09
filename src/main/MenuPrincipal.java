a0ba72eb06ae2bde36f5f91f09ce0f23ce583e79
beneditorodrigo <rodrigodacunha7@gmail.com>
2022-08-09 15:55:55 -0300

    Fazendo alterações em Menu

--- src/main/MenuPrincipal.java
+++ src/main/MenuPrincipal.java
@@ -23,11 +23,48 @@ public class MenuPrincipal extends JFrame implements ActionListener {
 	Horarios horarios;
 	JMenu menuHorarios, menuMarcacoes, menuAtrasos, menuHorasExtras, menuGerenciamento;
 	JButton i1, i2, i3, i4, i5, i6;
+	
+	public void actionPerformed(ActionEvent e) {
+		
+		if(e.getSource() == i1) {
+			setVisible(false);
+			InserirHorarioDeTrabalho inserirHorarioDeTrabalho = new InserirHorarioDeTrabalho(horarios.getHorariosDeTrabalho(), horarios);
+		}
+		
+		if(e.getSource() == i2) {
+			setVisible(false);
+			InserirMarcacoes marcacoes = new InserirMarcacoes(horarios.getMarcacoesFeitas(), horarios);
+		}
+
+		if(e.getSource() == i3) {
+			setVisible(false);
+			Atrasos atrasos = new Atrasos(horarios);
+		}
+		
+		if(e.getSource() == i4) {
+			setVisible(false);
+			HorasExtras horasExtras = new HorasExtras(horarios);
+		}
+		
+		if(e.getSource() == i5) {
+			ResetDosDados resetDados = new ResetDosDados();
+			boolean retorno = resetDados.limparDados(horarios);
+			if(retorno) {
+				JOptionPane.showMessageDialog(null, "Dados Apagados");
+			} else {
+				JOptionPane.showMessageDialog(null, "ERRO");
+			}
+		}
+		
+		if(e.getSource() == i6) {
+			System.exit(0);
+		}
+	}
 
 	public MenuPrincipal(Horarios horarios) {
 		this.horarios = horarios;
 		
-		JFrame f = new JFrame("Menu Principal");
+		JFrame frame = new JFrame("Menu Principal");
 		JMenuBar menuBar = new JMenuBar();
 		
 		menuHorarios = new JMenu("Horarios");
@@ -58,49 +95,19 @@ public class MenuPrincipal extends JFrame implements ActionListener {
 		menuGerenciamento.add(i5);
 		menuGerenciamento.add(i6);
 
+		
+		
 		menuBar.add(menuHorarios);
 		menuBar.add(menuMarcacoes);
 		menuBar.add(menuAtrasos);
 		menuBar.add(menuHorasExtras);
 		menuBar.add(menuGerenciamento);
 		
-		f.setJMenuBar(menuBar);
-		f.setSize(500, 500);
-		f.setLocationRelativeTo(null);
-		f.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		f.setVisible(true);
-	}
-
-	public void actionPerformed(ActionEvent e) {
-	
-		if(e.getSource() == i1) {
-			InserirHorarioDeTrabalho inserirHorarioDeTrabalho = new InserirHorarioDeTrabalho(horarios.getHorariosDeTrabalho(), horarios);
-		}
-		
-		if(e.getSource() == i2) {
-			InserirMarcacoes marcacoes = new InserirMarcacoes(horarios.getMarcacoesFeitas(), horarios);
-		}
-
-		if(e.getSource() == i3) {
-			Atrasos atrasos = new Atrasos(horarios);
-		}
-		
-		if(e.getSource() == i4) {
-			HorasExtras horasExtras = new HorasExtras(horarios);
-		}
-		
-		if(e.getSource() == i5) {
-			ResetDosDados resetDados = new ResetDosDados();
-			boolean retorno = resetDados.limparDados(horarios);
-			if(retorno) {
-				JOptionPane.showMessageDialog(null, "Dados Apagados");
-			} else {
-				JOptionPane.showMessageDialog(null, "ERRO");
-			}
-		}
-		
-		if(e.getSource() == i6) {
-			System.exit(0);
-		}
+		frame.setJMenuBar(menuBar);
+		frame.setSize(500, 500);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		frame.setLayout(null);
+		frame.setVisible(true);
 	}
 }
